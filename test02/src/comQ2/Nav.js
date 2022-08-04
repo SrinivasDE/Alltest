f43287af7c891da4a98d3da43f81dbd08dc94ee8
SrinivasDE <srinivas.pydev@gmail.com>
2022-08-04 21:06:39 +0530

    i have completed Q3

---
+++ test02/src/comQ2/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <div className='container'>
+        <div className='row'>
+            <div className='col'>
+                
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Nav;

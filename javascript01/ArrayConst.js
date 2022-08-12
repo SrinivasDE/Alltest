61a6918c9319143419a037a863305ff0dbd2dd6d
SrinivasDE <srinivas.pydev@gmail.com>
2022-08-12 13:05:49 +0530

    Array Const in JS

---
+++ javascript01/ArrayConst.js
@@ -0,0 +1,15 @@
+// You can create a constant array:
+const cars = ["Saab", "Volvo", "BMW"];
+console.log(cars);
+// You can change an element:
+cars[0] = "Toyota";
+
+// You can add an element:
+cars.push("Audi");
+console.log(cars)
+
+{
+    const cars = ['a', 'b', 'c'];
+    console.log('inside cars',cars);
+}
+
